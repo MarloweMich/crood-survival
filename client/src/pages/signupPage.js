f260545304005ee43c63d8b231551d59757c3d91
Matthew Tanner <matthew.j.tanner@gmail.com>
2022-06-12 16:40:12 -0400

    recovery

---
+++ client/src/pages/signupPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Signup from "../components/signup";
+import "./signupPage.css";
+
+function SignupPage() {
+
+  return (
+    <>
+      <div className="formBox">
+        <Signup />
+      </div>
+    </>
+  );
+}
+
+export default SignupPage;
--- client/src/pages/winPage.js
+++ client/src/pages/winPage.js
@@ -3,16 +3,17 @@ import "./win.css";
 
 function WinPage() {
 
-    function changeLocation() {
-      window.location.assign('')
-    }
-
   return (
     <div className="container">
       <h1>Congratulations!</h1>
-      <button className="btn btn-lg btn-light m-2" onClick={()=>changeLocation()}>
+      <button className="btn btn-lg btn-light m-2" onClick="changeLocation">
         Play Again?
       </button>
+      <script>
+        function changeLocation() {
+          window.location.assign('/')
+        }
+      </script>
     </div>
   );
 }
