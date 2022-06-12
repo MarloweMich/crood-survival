7a1c8235699eaf6e9f21df6dbd4c2aa61e4b3b33
Matthew Tanner <matthew.j.tanner@gmail.com>
2022-06-12 10:32:23 -0400

    Interim, CSS

--- client/src/pages/losePage.js
+++ client/src/pages/losePage.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./win.css";
+//import { Link } from 'react-router-dom';
+import "./losePage.css";
 
 function LosePage() {
 
   return (
     <div className="container">
-      <h1>Congratulations!</h1>
+      <h1>Condolances!</h1>
       <button className="btn btn-lg btn-light m-2" onClick="changeLocation">
         Play Again?
       </button>
@@ -18,4 +19,4 @@ function LosePage() {
   );
 }
 
-export default WinPage;
+export default LosePage;
