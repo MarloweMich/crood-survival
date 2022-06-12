f070b8024ea32947f9da84d2e5fa40c83ca24d25
Matthew Tanner <matthew.j.tanner@gmail.com>
2022-06-12 18:39:41 -0400

    recovery

---
+++ client/src/pages/landingpage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginSignupSelect from "../components/loginSignupSelect";
+import "./landingpage.css";
+
+function LandingPage() {
+
+  return (
+    <>
+      <div className="formBox">
+        <LoginSignupSelect />
+      </div>
+    </>
+  );
+}
+
+export default LandingPage;
---
+++ client/src/pages/loginPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "../components/login";
+import "./loginPage.css";
+
+function LoginPage() {
+
+  return (
+    <>
+      <div className="formBox">
+        <Login />
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
